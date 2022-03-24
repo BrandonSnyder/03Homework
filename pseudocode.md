9e5caf3e731df045a43931ae0b21ea2e2082002d
Brandon Snyder <brandon.snyder019@gmail.com>
2022-03-24 09:14:38 -0400

    tested code against grading criteria and found some minor improvements. Code is complete.

--- pseudocode.md
+++ pseudocode.md
@@ -1,11 +1,28 @@
-Goal randomly generate a password with all or one of the following: lowercase, uppercase, numbers, and/or special characters.
+
+Just some note to get my brain going. 
+
+Goal randomly generate a password with all or atleast one of the following: lowercase, uppercase, numbers, and/or special characters.
+
+
+
+Create check function to verify correct amount of characters
+Create check function to verify use of all selected characters
+
 
 arrays of each
 random number selector
-pop up with selection of characters. 
-pop ups asking if the user needs each of the types in the password.
+for loop to compare arrays
+converting string to array for comparisons
+pop up with selection of characters. window.prompt()
+pop ups asking if the user needs each of the types in the password. window.confirm()
 
 
+Flow:
 
-
-Needs to have a check at end to verify one value from each of the selected categories. 
+Declare global variables
+Ask for length
+Check input
+Create pool of characters to pick from
+Pick characters
+Verify all selected character catagories are used
+output new password to screen
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+// Global Variables
 var generateBtn = document.querySelector("#generate");
 // Declaration of all possible characters in an Object
 var characters = {
@@ -10,46 +11,108 @@ var characters = {
   
   specialCharacters : ['!','@','#','$','%','^','&','*','?','<','>'],
 };
-// Pool of Characters where the password will be pulled from
-
-
+// stored decisions
+var lowChars=false;
+var upChars=false;
+var numChars=false;
+var specChars=false;
+var lengthOfPassword = 0 ;
+var randomPassword = ""; //Password 
+var poolOfChars = []; // array of all characters to choose password from
 
 // Declaration of function Wrap all code in this function
 function generatePassword(){
-var randomPassword = ""; 
-var poolOfChars = []; 
 //function to prompt number of characters return input as number
 function numOfChars(){
-  var lengthOfPassword = window.prompt('How many characters does your password need?', 'Enter a number between 1 and 30');
-  return lengthOfPassword;
+  var lengthOfPassword = window.prompt('How many characters does your password need?', 'Enter a number between 8 and 128.');
+  if(lengthOfPassword > 7 && lengthOfPassword < 129){
+    return lengthOfPassword;
+  }else{
+    window.alert('You entered and invalid response please try again.')
+    return numOfChars();
+  }
 };
-var lengthOfPassword = numOfChars();
+lengthOfPassword = numOfChars();
 
 // deciding characters return poolOfChars
 function chooseCharTypes(){
+  poolOfChars = []
   if(confirm("Click OK if you want to include lowercase letters in your password")){
     poolOfChars = poolOfChars.concat(characters.lowerCaseLetters);
+    lowChars=true
   };
 
   if(confirm("Click OK if you want to include uppercase letters in your password")){
     poolOfChars = poolOfChars.concat(characters.upperCaseLetters);
-  };
+    upChars=true;};
+  
   if(confirm("Click OK if you want to include numbers in your password")){
     poolOfChars = poolOfChars.concat(characters.nums);
+    numChars=true;
   };
   if(confirm("Click OK if you want to include special characters in your password")){
     poolOfChars = poolOfChars.concat(characters.specialCharacters);
+    specChars= true;
   };
+  if(!lowChars && !upChars && !numChars && !specChars){
+    window.alert('you need to select at least 1 type of character to proceed!')
+    return chooseCharTypes();
+  }
   return poolOfChars;
 };
 poolOfChars = chooseCharTypes();
 
-// deciding on an index
-// creating the password
 
+// creating the password based off of poolOfChars
+function initPassword(){
+  randomPassword=[]
 for ( i = 0; i < lengthOfPassword ; i++ ) {
   randomPassword = randomPassword + poolOfChars[Math.floor(Math.random()*poolOfChars.length)];
+};};
+
+// Comparing string and array that are passed into function return true or calls initPassword to get a new password. 
+function findCommonElement(string,array){
+  var stringConverted = [];
+//  converts a string to an array
+  for( b = 0; b < string.length;b++){
+    stringConverted[b]=string[b];
+  }
+// compares individual letter from string to array 
+  for(i=0; i < stringConverted.length; i++){
+    for(j=0; j < array.length; j++){
+      if (array[i] === stringConverted[j]){
+      return true;
+    }else{
+      initPassword();
+    };
+  }}};
+
+
+function checkPassword(){
+  // check for lower case letters
+  if(lowChars){
+    if(findCommonElement(randomPassword,characters.lowerCaseLetters)){
+    }else{initPassword()
+  };};
+
+  if(upChars){
+    if(findCommonElement(randomPassword,characters.upperCaseLetters)){
+    }else{initPassword()
+  };};
+  if(numChars){
+    if(findCommonElement(randomPassword,characters.nums)){
+    }else{initPassword()
+  };};
+  if(specChars){
+    if(findCommonElement(randomPassword,characters.specialCharacters)){
+    }else{initPassword()
+  };};
 };
+
+console.log(randomPassword);
+initPassword()
+checkPassword()
+
 return randomPassword;
 }
 
